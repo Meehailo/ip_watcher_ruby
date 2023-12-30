0e570168d80584239402996de23426ff29a6001c
Mikhail Litvinenko <mikhail.litvinenko@balance-pl.ru>
2023-12-30 11:24:11 +0700

    open ai integration

---
+++ analyze_code.py
@@ -0,0 +1,25 @@
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def analyze_code_with_chatgpt(code_snippet):
+    try:
+        response = openai.Completion.create(
+          model="gpt-4",
+          prompt=f"Review the following code and provide suggestions for improvement:\n\n{code_snippet}",
+          temperature=0.7,
+          max_tokens=150
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+code_snippet = """
+def example_function():
+    print("This is an example function.")
+"""
+
+result = analyze_code_with_chatgpt(code_snippet)
+print("Suggestions from ChatGPT:\n", result)
