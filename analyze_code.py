08c462f80b9723f6181d35e8a50c5655960b78c7
Mikhail Litvinenko <mikhail.litvinenko@balance-pl.ru>
2023-12-30 11:27:45 +0700

    version openai

--- analyze_code.py
+++ analyze_code.py
@@ -6,7 +6,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def analyze_code_with_chatgpt(code_snippet):
     try:
         response = openai.Completion.create(
-          model="gpt-4",
+          model="text-davinci-003",
           prompt=f"Review the following code and provide suggestions for improvement:\n\n{code_snippet}",
           temperature=0.7,
           max_tokens=150
